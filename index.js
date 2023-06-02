cc08ceac97284bb597be90134b021b719318940d
Joel <ndohjoelmbj16@gmail.com>
2023-06-02 14:35:52 +0100

    I have reduced the number of for loop

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const en = require("./locale/en");
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs");
+const path = require("path");
 
-function checkFormat(number){
+let jsonData;
+
+function checkFormat(number) {
   // Regex pattern to match +234 followed by 10 digits or 0 followed by 10 digits
   const pattern = /^(?:0\d{10})$/;
 
@@ -10,54 +12,53 @@ function checkFormat(number){
   if (pattern.test(number)) {
     return true;
   } else {
-    if (number.length != 11 && number.startsWith('0')) {
+    if (number.length != 11 && number.startsWith("0")) {
       return en.invalid_without_dial_code;
     } else {
       return en.invalid_phone_number_format;
     }
   }
-
 }
 
-async function searchJSONFile(prefix){
-    const filePath = path.join(__dirname, "numberPrefix.json")
+async function searchJSONFile(prefix) {
+  if (jsonData) {
+    return jsonData[prefix];
+  } else {
+    const filePath = path.join(__dirname, "numberPrefix.json");
 
     try {
-        const fileData = await fs.promises.readFile(filePath, 'utf-8');
-        const jsonData = JSON.parse(fileData);
-        return jsonData[prefix]
-      } catch (error) {
-        console.error('Error reading JSON file:', error);
-        return null;
-      }
+      const fileData = await fs.promises.readFile(filePath, "utf-8");
+      jsonData = JSON.parse(fileData);
+      return jsonData[prefix];
+    } catch (error) {
+      console.error("Error reading JSON file:", error);
+      return null;
+    }
+  }
 }
 
-async function detectNumberIssuer(number){
-    let format = checkFormat(number)
+async function detectNumberIssuer(number) {
+  let format = checkFormat(number);
 
-    if(format === true){
-        const firstFiveChars = number.substring(0, 5);
+  if (format == true) {
 
-        let numIssuer = await searchJSONFile(firstFiveChars);
-        
-        if(numIssuer != undefined){
-            return {status: true, message: numIssuer}
-        }
-        else{
-            const firstFourChars = number.substring(0, 4);
-            let numIssuer = await searchJSONFile(firstFourChars);
-            
-            if(numIssuer != undefined){
-                return {status: true, message: numIssuer}
-            }
-            
-        }
-        return {status: false, message: en.no_issuer_found}
-        
+    return checkIssuer(number);
+
+    async function checkIssuer(number) {
+      const firstFiveChars = number.substring(0, 5);
+      const firstFourChars = number.substring(0, 4);
+
+      let numIssuer =
+        (await searchJSONFile(firstFiveChars)) ||
+        (await searchJSONFile(firstFourChars));
+
+      return numIssuer
+        ? { status: true, message: numIssuer }
+        : { status: false, message: en.no_issuer_found };
     }
-    else{
-        return {status: false, message: format}
-    }
+  }
+
+  return { status: false, message: format };
 }
 
-module.exports = {detectNumberIssuer}
+module.exports = { detectNumberIssuer };
