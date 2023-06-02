e6da5a3766643ded0316a8c766256e9479592b54
Barbara Odozi <barbs.nene@gmail.com>
2023-06-02 16:37:07 +0100

    Update .gitignore and remove node_modules

--- node_modules/@japa/core/build/src/interpolate.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A simple function interpolate values inside curly braces.
- *
- * @example
- * interpolate('hello { username }', { username: 'virk' })
- */
-export declare function interpolate(input: string, data: any, index: number): string;
