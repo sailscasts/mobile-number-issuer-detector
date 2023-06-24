3ed95da40a815d8c6c4a0e48ea712317e6f2aeda
Neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2023-06-24 20:43:36 +0530

    Update index.js
    feat: removed comments self-explanatory variables

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ function resetInput(field) {
 
 // check if DOM is loaded first
 document?.addEventListener('DOMContentLoaded', () => {
-  // get all inputs by ID
   const numberInput = document.getElementById('numberInput');
   const detectButton = document.getElementById('detectButton');
   const resultElement = document.getElementById('result');
@@ -16,12 +15,12 @@ document?.addEventListener('DOMContentLoaded', () => {
 
   // check if button is loaded in DOM first
   detectButton?.addEventListener('click', () => {
-    const number = numberInput.value; // get the value of input
+    const number = numberInput.value; 
 
     const csvFile = csv.value
 
     if (number) {
-      const provider = predictDigit(number); // send to algorithm
+      const provider = predictDigit(number); 
 
       resultElement.textContent = `${provider} - ${number}`; // send response to html
 
