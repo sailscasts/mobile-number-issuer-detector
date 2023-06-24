0e7ec9d0cf1545b09fa90232f12739e7c7735ea9
Lennox Charles <lennyaiko17@gmail.com>
2023-06-24 14:57:43 +0100

    feat: upload file for processing

--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
 import { predictDigit } from './predict-digit.js';
 
+function resetInput(field) {
+  if (field.value !== '') {
+    field.value = '';
+  }
+}
+
 // check if DOM is loaded first
 document?.addEventListener('DOMContentLoaded', () => {
   // get all inputs by ID
   const numberInput = document.getElementById('numberInput');
   const detectButton = document.getElementById('detectButton');
   const resultElement = document.getElementById('result');
+  const csv = document.getElementById('csvFile');
 
   // check if button is loaded in DOM first
   detectButton?.addEventListener('click', () => {
     const number = numberInput.value; // get the value of input
 
-    const provider = predictDigit(number); // send to algorithm
+    const csvFile = csv.value
 
-    resultElement.textContent = `${provider} - ${number}`; // send response to html
+    if (number) {
+      const provider = predictDigit(number); // send to algorithm
 
-    if (numberInput.value !== '') {
-      numberInput.value = '';
-    } // reset input
+      resultElement.textContent = `${provider} - ${number}`; // send response to html
+
+      resetInput(numberInput) // reset input
+    }
+    
+    else if (csvFile) {
+      console.log('File!')
+      resetInput(csv)
+    }
+
+    else {
+      resultElement.textContent = 'Please provide a value to detect';
+    }
+
+
+    
   });
 });
