38e57608036599356e43d70045c75ff548ae9920
Vulpes231 <whykayshot@gmail.com>
2023-06-26 18:57:42 -0400

    fix: Revamped the frontend

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { predictDigit } from './predict-digit.js';
+import {predictDigit} from './predict-digit.js';
 
 document?.addEventListener('DOMContentLoaded', () => {
   const numberInput = document.getElementById('numberInput');
@@ -8,32 +8,28 @@ document?.addEventListener('DOMContentLoaded', () => {
 
   // check if button is loaded in DOM first
   detectButton?.addEventListener('click', () => {
-
     if (numberInput.value) {
-      const provider = predictDigit(numberInput.value); 
+      const provider = predictDigit(numberInput.value);
 
+      resultElement.style.color = 'green';
       resultElement.textContent = `${provider} - ${numberInput.value}`; // send response to html
 
-      numberInput.value = '' // reset input
-      document.getElementById('thead').innerHTML = ''
-      document.getElementById('tbody').innerHTML = ''
-    }
-    
-    else if (csv.value) {
-      const reader = new FileReader()
+      numberInput.value = ''; // reset input
+      document.getElementById('thead').innerHTML = '';
+      document.getElementById('tbody').innerHTML = '';
+    } else if (csv.value) {
+      const reader = new FileReader();
       reader.onload = () => {
-        const numbers = d3.csvParse(reader.result)
-        document.getElementById('result').innerHTML = ''
-        toHtml(numbers)
-      }
-      if(csvFile.files[0]) reader.readAsText(csvFile.files[0])
-      csv.value = ''
-    }
-
-    else {
+        const numbers = d3.csvParse(reader.result);
+        document.getElementById('result').innerHTML = '';
+        toHtml(numbers);
+      };
+      if (csvFile.files[0]) reader.readAsText(csvFile.files[0]);
+      csv.value = '';
+    } else {
+      resultElement.style.color = 'red';
       resultElement.textContent = 'Please provide a value to detect';
     }
-
   });
 });
 
@@ -42,16 +38,37 @@ function toHtml(obj) {
   <th>S/N</th>
   <th>Number</th>
   <th>ISP</th>
-  `
-  document.getElementById('thead').innerHTML += thead
+  `;
+  document.getElementById('thead').innerHTML += thead;
   for (let i = 0; i < obj.length; i++) {
     let row = `
       <tr>
         <td>${obj[i].id}</td>
-        <td>${'0'+obj[i].Number}</td>
-        <td>${predictDigit('0'+obj[i].Number)}</td>
+        <td>${'0' + obj[i].Number}</td>
+        <td>${predictDigit('0' + obj[i].Number)}</td>
       </tr>
-    `
-    document.getElementById('tbody').innerHTML += row
+    `;
+    document.getElementById('tbody').innerHTML += row;
   }
 }
+
+// hamburger button function
+const menuBtn = document.getElementById('menu-button');
+
+menuBtn.addEventListener('click', function () {
+  const links = document.getElementById('nav-links');
+  links.classList.toggle('show-menu');
+  if (links.classList.contains('show-menu')) {
+    menuBtn.innerHTML = `
+      <span class="material-symbols-outlined">
+        close
+      </span>
+    `;
+  } else {
+    menuBtn.innerHTML = `
+      <span class="material-symbols-outlined">
+        menu
+      </span>
+    `;
+  }
+});
